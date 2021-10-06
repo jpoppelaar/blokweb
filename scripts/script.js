c6e1bcc415a777b123d373a139c7ddda97892744
jpoppelaar <77064774+jpoppelaar@users.noreply.github.com>
2021-10-06 11:54:57 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var hamburgermenu = document.querySelector('.hamburgermenu');
+var navigatie = document.querySelector('.navigatie');
+
+hamburgermenu.addEventListener('click',menuUitklappen);
+
+function menuUitklappen(){
+navigatie.classList.toggle('uitklappen');
+}
